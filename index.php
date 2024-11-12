b905a6304c787eef4cd048f5e5d7127526c6d8c6
bry4nG4rrix <bryanmfb4@gmail.com>
2024-11-12 20:27:57 +0300

    blog

--- index.php
+++ index.php
@@ -14,11 +14,38 @@ if(empty($uname)){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>G_blog</title>
+    <style>
+        a{
+            color: white;
+        }
+        .navb{
+            border-radius: 20px;
+        }
+    </style>
 </head>
 <body>
-    <p>tongasoa ianao ry : <?php echo $uname; ?></p>
-    <a href="decon.php"><input type="button" value="Deconnexion"></a>
+   <nav class=" navb navbar navbar-expand-sm bg-success m-2">
+        <div class="container-fluid">
+            <p class="h6 text-light">Hello <?php echo $uname ; ?></p>
+            <ul class="navbar-nav justify-content-center">
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-light">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-light">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-light">Contact</a>
+                </li>
+            </ul>
+            <a href="decon.php" class="nav-link"><input type="button" class="btn btn-light text-success" value="Deconnexion"></a>
+
+        </div>
+   </nav>
+    
 </body>
 </html>
